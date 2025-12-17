1b273ef09ff361ca3a63c8be1c28716947c318cd
0xv1n4y <vinaysurisetty02@gmail.com>
2025-12-17 12:26:44 +0530

    Update production URL

--- src/global/index.js
+++ src/global/index.js
@@ -1,24 +1,37 @@
 import axios from "axios";
-import { urls } from "./utils";
-
 axios.defaults.withCredentials = true;
 
+const getApiHost = () => {
+  // Production (Vercel)
+  if (process.env.REACT_APP_API_URL) {
+    return process.env.REACT_APP_API_URL;
+  }
+
+  // Local fallback
+  return "http://localhost:4000";
+};
+
+export const API_HOST = getApiHost();
+
 export const GLOBALS = {
-    api_login : urls.apiHost + '/api/v1/login', // POST (for logging in)
-    api_newuser : urls.apiHost + '/api/v1/newuser/add',  // POST (for creating a new user)
-    api_fetchtoken: urls.apiHost + '/api/v1/details',
-    api_logout: urls.apiHost + '/api/v1/logout',
-    api_usersemedia : urls.apiHost + '/media/users/',
-    api_getusers : urls.apiHost + '/api/v1/users',
-    api_createchat : urls.apiHost + '/api/v1/chat',
-    api_fecthchats : urls.apiHost + '/api/v1/chats',
-    api_fetchmessages : urls.apiHost + '/api/v1/messages',
-    api_sendnewmessage : urls.apiHost + '/api/v1/messages/new',
-    api_creategroup : urls.apiHost + '/api/v1/group/new',
-    api_leavegroup : urls.apiHost + '/api/v1/group',
-    api_updategroupname: urls.apiHost + '/api/v1/groups/',   // PUT (for updating group name)
-    api_addmembers: urls.apiHost + '/api/v1/groups/', // POST (for adding members to a group)
-    api_removemembers: urls.apiHost + '/api/v1/groups/',  // POST or PUT (for leaving a group)
-}
+  api_login: `${API_HOST}/api/v1/login`,
+  api_newuser: `${API_HOST}/api/v1/newuser/add`,
+  api_fetchtoken: `${API_HOST}/api/v1/details`,
+  api_logout: `${API_HOST}/api/v1/logout`,
 
+  api_usersemedia: `${API_HOST}/media/users/`,
+  api_getusers: `${API_HOST}/api/v1/users`,
 
+  api_createchat: `${API_HOST}/api/v1/chat`,
+  api_fecthchats: `${API_HOST}/api/v1/chats`,
+
+  api_fetchmessages: `${API_HOST}/api/v1/messages`,
+  api_sendnewmessage: `${API_HOST}/api/v1/messages/new`,
+
+  api_creategroup: `${API_HOST}/api/v1/group/new`,
+  api_leavegroup: `${API_HOST}/api/v1/group`,
+
+  api_updategroupname: `${API_HOST}/api/v1/groups/`,
+  api_addmembers: `${API_HOST}/api/v1/groups/`,
+  api_removemembers: `${API_HOST}/api/v1/groups/`,
+};
